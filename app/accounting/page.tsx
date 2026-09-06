64752c2743328c335372798e8e48abe70bca8ea1
Kacper Bernecki <ft4k696bk6@privaterelay.appleid.com>
2026-09-06 23:53:14 +0200

    Disable unused accounting workspace

--- app/accounting/page.tsx
+++ app/accounting/page.tsx
@@ -1,6 +1,5 @@
-import { DepartmentWorkspacePage } from "@/components/department-workspace-page";
+import { redirect } from "next/navigation";
 
 export default function AccountingPage() {
-  return <DepartmentWorkspacePage kind="accounting" />;
+  redirect("/realizacja/umowy");
 }
-
